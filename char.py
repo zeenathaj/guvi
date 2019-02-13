d0b7bf5e4bc10a5825a4d013e8a6d23ef73e9832
zeenathaj <47587786+zeenathaj@users.noreply.github.com>
2019-02-13 14:32:54 +0530

    Update and rename num.py to char.py

---
+++ char.py
@@ -0,0 +1,11 @@
+ch = int(input())
+if (ch=='A' or ch=='a' ch=='E' or ch=='e' ch=='I' or ch=='i' ch=='O' or ch=='o' ch=='U' or ch=='u'):
+	print(ch, "Vowel")
+elif:
+	print(ch, "Constant")
+else:
+	print("Invalid")
+
+
+	
+	
